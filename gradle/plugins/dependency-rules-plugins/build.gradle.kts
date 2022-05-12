de06826fe02173c99d7df894fe61a14cf6038de3
Jendrik Johannes <jendrik.johannes@gmail.com>
2022-05-12 09:09:37 +0200

    Dependency rules

--- gradle/plugins/dependency-rules-plugins/build.gradle.kts
+++ gradle/plugins/dependency-rules-plugins/build.gradle.kts
@@ -1,3 +1,11 @@
 plugins {
     `kotlin-dsl`
 }
+
+dependencies {
+    implementation(platform(project(":plugins-platform")))
+
+    implementation("de.jjohannes.gradle:java-ecosystem-capabilities")
+    implementation("de.jjohannes.gradle:missing-metadata-guava")
+    implementation("dev.jacomet.gradle.plugins:logging-capabilities")
+}
--- gradle/plugins/plugins-platform/build.gradle.kts
+++ gradle/plugins/plugins-platform/build.gradle.kts
@@ -1,3 +1,9 @@
 plugins {
     id("java-platform")
 }
+
+dependencies.constraints {
+    api("de.jjohannes.gradle:java-ecosystem-capabilities:0.4")
+    api("de.jjohannes.gradle:missing-metadata-guava:31.1.1")
+    api("dev.jacomet.gradle.plugins:logging-capabilities:0.10.0")
+}
