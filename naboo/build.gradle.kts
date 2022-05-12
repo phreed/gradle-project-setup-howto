d88ec99f6e6ec5c012dd1f4b3372ffa217af9754
Jendrik Johannes <jendrik.johannes@gmail.com>
2022-05-12 11:15:12 +0200

    Declare dependencies (in 'build.gradle.kts' files)

--- naboo/build.gradle.kts
+++ naboo/build.gradle.kts
@@ -3,5 +3,11 @@ plugins {
 }
 
 dependencies {
+    implementation("org.apache.solr:solr-solrj")
+    implementation("org.apache.zookeeper:zookeeper-jute")
+
+    testImplementation("junit:junit")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine") // Some JUnit4 tests still around only in this project, use 'vintage engine' to run them
 }
--- tatooine/build.gradle.kts
+++ tatooine/build.gradle.kts
@@ -5,3 +5,11 @@ plugins {
 tasks.test {
     useJUnit()  // We run the old JUnit4 tests directly with JUnit4 because of _some obscure test setup reason_
 }
+
+dependencies {
+    api("com.github.racc:typesafeconfig-guice")
+
+    implementation("org.apache.zookeeper:zookeeper")
+
+    testImplementation("junit:junit")
+}
