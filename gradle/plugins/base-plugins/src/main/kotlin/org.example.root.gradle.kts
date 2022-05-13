d9d354acf4bbcfef72eac29bce2fa48e7faf4ba8
Jendrik Johannes <jendrik.johannes@gmail.com>
2022-05-13 14:28:17 +0200

    Lifecycle tasks in root

--- gradle/plugins/base-plugins/src/main/kotlin/org.example.root.gradle.kts
+++ gradle/plugins/base-plugins/src/main/kotlin/org.example.root.gradle.kts
@@ -1,2 +1,37 @@
 plugins {
 }
+
+// Configure the ':tasks' task of the root project to only show
+// the main lifecycle tasks as entry points to the build
+val mainBuildGroup = "main build"
+tasks.named<TaskReportTask>("tasks") {
+    displayGroup = mainBuildGroup
+}
+
+tasks.register("build") {
+    group = mainBuildGroup
+    description = "Complete build of all modules and the application"
+    dependsOn(subprojects.map { ":${it.name}:$name" })
+}
+
+tasks.register("check") {
+    group = mainBuildGroup
+    description = "Runs all checks and produces test summary and code coverage reports"
+    dependsOn(subprojects.map { ":${it.name}:$name" })
+    doLast {
+        println("Unit test summary: app/build/reports/tests/unit-test/aggregated-results/index.html")
+        println("Unit test code coverage: app/build/reports/jacoco/testCodeCoverageReport/html/index.html")
+    }
+}
+
+tasks.register("run") {
+    group = mainBuildGroup
+    description = "Build and run as standalone application"
+    dependsOn(":app:$name")
+}
+
+tasks.register("deployWebApp") {
+    group = mainBuildGroup
+    description = "Deploy web app into local Tomcat found via CATALINA_HOME"
+    dependsOn(":app:$name")
+}
