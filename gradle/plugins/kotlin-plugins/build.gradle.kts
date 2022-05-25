19b146a8614fef36e646785e0f0f14a6e2268691
fred eisele <fred.eisele@vanderbilt.edu>
2022-05-25 15:23:40 -0500

    make the kotlin version match that of the kotlin-dsl plugin

--- gradle/plugins/kotlin-plugins/build.gradle.kts
+++ gradle/plugins/kotlin-plugins/build.gradle.kts
@@ -4,10 +4,6 @@ plugins {
 
 dependencies {
     implementation(platform(project(":plugins-platform")))
-    
     implementation(project(":base-plugins"))
-
-    // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-gradle-plugin
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
 }
